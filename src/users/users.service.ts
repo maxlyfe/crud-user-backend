1bda9b8cfaa60116e41e4095bfa1bcb467cd8dcb
MaxLyFe <85650989+maxlyfe@users.noreply.github.com>
2021-10-26 22:42:20 -0300

    set

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -23,7 +23,8 @@ export class UsersService {
   }
 
   update(id: string, updateUserDto: UpdateUserDto) {
-    return this.userModel.findByIdAndUpdate({
+    return this.userModel.findByIdAndUpdate(
+      {
         _id: id,
       },
       {
