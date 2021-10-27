4e32f625ed1c2047d452f7f237c5888cf7c268ba
MaxLyFe <85650989+maxlyfe@users.noreply.github.com>
2021-10-26 22:25:49 -0300

    Pronto pra pdocucao

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
+    MongooseModule.forRoot('mongodb+srv://User:xeT82cflkFaABco2@nestjscrud.palng.mongodb.net/test'),
     UsersModule],
   controllers: [],
   providers: [],
